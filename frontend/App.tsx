e8d25d7536c854d642f8073e55ba8cd83c12241a
ZacharyWM <zacharywmoorman@gmail.com>
2025-03-06 07:59:44 -0600

    update

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import Home from "./pages/page";
-// import "./style/parsing-styles.css";
-// import "./style/style.css";
 
 const App = () => {
   return (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-// const { nextui } = require("@nextui-org/react");
-// import { customThemes } from "./frontend/style/tailwind-custom-themes";
-// import { darkTheme, lightTheme } from "./frontend/style/tailwind-custom-themes";
 
 module.exports = {
-  content: [
-    "./frontend/**/*.{html,js,ts,tsx}",
-
-    // The following line seems unnecessary:
-    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./frontend/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -94,26 +86,4 @@ module.exports = {
     },
   },
   darkMode: "class",
-  //   plugins: [
-  //     nextui({
-  //       prefix: "nextui", // prefix for themes variables
-  //       addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-  //       defaultTheme: darkTheme, // default theme from the themes object
-  //       defaultExtendTheme: darkTheme, // default theme to extend on custom themes
-  //       layout: {
-  //         disabledOpacity: "0.3",
-  //         radius: {
-  //           small: "1px",
-  //           medium: "2px",
-  //           large: "4px",
-  //         },
-  //         borderWidth: {
-  //           small: "1px",
-  //           medium: "2px",
-  //           large: "3px",
-  //         },
-  //       }, // common layout tokens (applied to all themes)
-  //       //   themes: customThemes,
-  //     }),
-  //   ],
 };
