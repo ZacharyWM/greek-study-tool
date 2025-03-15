d531da6352b69cba16602f26690d93299e730ef3
ZacharyWM <zacharywmoorman@gmail.com>
2025-03-14 20:43:52 -0500

    add jwt validation support

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ZacharyWM/greek-study-tool
 go 1.23.0
 
 require (
+	github.com/auth0/go-jwt-middleware/v2 v2.3.0
 	github.com/coreos/go-oidc/v3 v3.12.0
 	github.com/gin-contrib/sessions v1.0.2
 	github.com/gin-gonic/gin v1.10.0
@@ -22,7 +23,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/gorilla/context v1.1.2 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/gorilla/sessions v1.2.2 // indirect
@@ -33,14 +33,15 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.12.0 // indirect
 	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/protobuf v1.36.0 // indirect
+	gopkg.in/go-jose/go-jose.v2 v2.6.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- platform/middleware/isAuthenticated.go
+++ platform/middleware/isAuthenticated.go
@@ -1,19 +1,120 @@
 package middleware
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
+	"github.com/ZacharyWM/greek-study-tool/platform/auth0const"
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
+
+type CustomClaims struct {
+	Scope string `json:"scope"`
+}
+
+// Needed to satify validator.CustomClaims interface.
+func (c CustomClaims) Validate(ctx context.Context) error {
+	return nil
+}
+
+// Needed to satify validator.CustomClaims interface.
+func (c CustomClaims) HasScope(expectedScope string) bool {
+	return false
+	// result := strings.Split(c.Scope, " ")
+	// for i := range result {
+	// 	if result[i] == expectedScope {
+	// 		return true
+	// 	}
+	// }
+
+	// return false
+}
 
 // IsAuthenticated is a middleware that checks if
 // the user has already been authenticated previously.
 func IsAuthenticated(ctx *gin.Context) {
 	if sessions.Default(ctx).Get("profile") == nil {
-		ctx.Status(http.StatusUnauthorized)
-		ctx.Abort()
+		ctx.Redirect(http.StatusSeeOther, "/")
 	} else {
 		ctx.Next()
 	}
+}
+
+// JwtAuth is a middleware that will check the validity of our JWT.
+func JwtAuth() gin.HandlerFunc {
+	jwtValidator := getJwtValidator()
+
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Authorization header missing or invalid"})
+			ctx.Abort()
+			return
+		}
+
+		token, err := jwtValidator.ValidateToken(
+			ctx.Request.Context(),
+			strings.TrimPrefix(authHeader, "Bearer "),
+		)
+		if err != nil {
+			log.Printf("encountered error while validating JWT: %v", err)
+			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Failed to validate JWT"})
+			ctx.Abort()
+			return
+		}
+
+		// TODO - probably don't care about this
+		claims := token.(*validator.ValidatedClaims)
+		ctx.Set("claims", claims)
+
+		// TODO - probably don't care about this
+		if customClaims, ok := claims.CustomClaims.(*CustomClaims); ok {
+			session := sessions.Default(ctx)
+			session.Set("auth_scope", customClaims.Scope)
+			session.Save()
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "Token is valid", "claims": claims})
+
+		// Continue to the next middleware/handler
+		// ctx.Next()
+	}
+}
+
+func getJwtValidator() *validator.Validator {
+	issuerURL, err := url.Parse("https://" + auth0const.AUTH0_DOMAIN + "/")
+	if err != nil {
+		log.Fatalf("Failed to parse the issuer url: %v", err)
+	}
+
+	provider := jwks.NewCachingProvider(issuerURL, 5*time.Minute)
+
+	jwtValidator, err := validator.New(
+		provider.KeyFunc,
+		validator.RS256,
+		issuerURL.String(),
+		[]string{
+			auth0const.AUTH0_AUDIENCE_1,
+			auth0const.AUTH0_AUDIENCE_2,
+		},
+		// TODO - can I get get of validator.WithCustomClaims?
+		validator.WithCustomClaims(
+			func() validator.CustomClaims {
+				return &CustomClaims{}
+			},
+		),
+		// TODO - what the heck is this?
+		validator.WithAllowedClockSkew(time.Minute),
+	)
+	if err != nil {
+		log.Fatalf("Failed to set up the jwt validator")
+	}
+	return jwtValidator
 }
