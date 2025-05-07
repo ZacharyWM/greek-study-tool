ea3d246199556e4d9cca3cb645d644693c2b9dde
ZacharyWM <zacharywmoorman@gmail.com>
2025-05-06 21:49:20 -0500

    update how we import book data

--- server/database/database.go
+++ server/database/database.go
@@ -12,14 +12,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-//go:embed books/1_Matthew.json
-// var matthew []byte
-
-// //go:embed books/2_Mark.json
-// var mark []byte
-
-// //go:embed books/27_Revelation.json
-// var revelation []byte
+// //go:embed books/*
+// var booksFS embed.FS
 
 var DB *sql.DB
 
@@ -57,125 +51,117 @@ func InitDB() {
 // TODO - work out a better migration strategy
 func RunMigrations() {
 	/*
-			createUsersTableCmd := `
-			CREATE TABLE IF NOT EXISTS users (
-				id SERIAL PRIMARY KEY,
-				idp_id VARCHAR(255) NOT NULL,
-				first_name VARCHAR(255),
-				last_name VARCHAR(255),
-				nickname VARCHAR(255),
-				name VARCHAR(255),
-				picture TEXT,
-				email VARCHAR(255),
-				email_verified BOOLEAN
-			);
+					createUsersTableCmd := `
+					CREATE TABLE IF NOT EXISTS users (
+						id SERIAL PRIMARY KEY,
+						idp_id VARCHAR(255) NOT NULL,
+						first_name VARCHAR(255),
+						last_name VARCHAR(255),
+						nickname VARCHAR(255),
+						name VARCHAR(255),
+						picture TEXT,
+						email VARCHAR(255),
+						email_verified BOOLEAN
+					);
 
-			CREATE UNIQUE INDEX IF NOT EXISTS idx_users_idp_id ON users(idp_id);
-			`
-			_, err := DB.Exec(createUsersTableCmd)
-			if err != nil {
-				log.Fatalf("Error creating users table: %v", err)
-			}
+					CREATE UNIQUE INDEX IF NOT EXISTS idx_users_idp_id ON users(idp_id);
+					`
+					_, err := DB.Exec(createUsersTableCmd)
+					if err != nil {
+						log.Fatalf("Error creating users table: %v", err)
+					}
 
-			createAnalyesesTableCmd := `
-				CREATE TABLE IF NOT EXISTS analyses (
-				id SERIAL PRIMARY KEY,
-				user_id INTEGER NOT NULL REFERENCES users(id),
-				details JSONB
-			);
+					createAnalyesesTableCmd := `
+						CREATE TABLE IF NOT EXISTS analyses (
+						id SERIAL PRIMARY KEY,
+						user_id INTEGER NOT NULL REFERENCES users(id),
+						details JSONB
+					);
 
-			CREATE INDEX IF NOT EXISTS idx_analyses_user_id ON analyses(user_id);
-			`
-			_, err = DB.Exec(createAnalyesesTableCmd)
-			if err != nil {
-				log.Fatalf("Error creating analyses table: %v", err)
-			}
+					CREATE INDEX IF NOT EXISTS idx_analyses_user_id ON analyses(user_id);
+					`
+					_, err = DB.Exec(createAnalyesesTableCmd)
+					if err != nil {
+						log.Fatalf("Error creating analyses table: %v", err)
+					}
 
-			addFieldsToAnalyesesTableCmd := `
-				ALTER TABLE analyses
-				ADD COLUMN title VARCHAR(100),
-				ADD COLUMN description TEXT,
-				ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-				ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
-			`
-			_, err = DB.Exec(addFieldsToAnalyesesTableCmd)
-			if err != nil {
-				slog.Debug("This is expected if the 'analyses table already exists")
-			}
+					addFieldsToAnalyesesTableCmd := `
+						ALTER TABLE analyses
+						ADD COLUMN title VARCHAR(100),
+						ADD COLUMN description TEXT,
+						ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+						ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+					`
+					_, err = DB.Exec(addFieldsToAnalyesesTableCmd)
+					if err != nil {
+						slog.Debug("This is expected if the 'analyses table already exists")
+					}
 
 
-		// Create Books table
-		createBooksTableCmd := `
-		CREATE TABLE IF NOT EXISTS books (
-			id SERIAL PRIMARY KEY,
-			title VARCHAR(255) NOT NULL
-		);
-		`
-		_, err := DB.Exec(createBooksTableCmd)
-		if err != nil {
-			slog.Debug("This is expected if the 'books' table already exists")
-		}
+				// Create Books table
+				createBooksTableCmd := `
+				CREATE TABLE IF NOT EXISTS books (
+					id SERIAL PRIMARY KEY,
+					title VARCHAR(255) NOT NULL
+				);
+				`
+				_, err := DB.Exec(createBooksTableCmd)
+				if err != nil {
+					slog.Debug("This is expected if the 'books' table already exists")
+				}
 
-		// Create Chapters table
-		createChaptersTableCmd := `
-		CREATE TABLE IF NOT EXISTS chapters (
-			id SERIAL PRIMARY KEY,
-			book_id INTEGER NOT NULL REFERENCES books(id),
-			number INTEGER NOT NULL
-		);
+				// Create Chapters table
+				createChaptersTableCmd := `
+				CREATE TABLE IF NOT EXISTS chapters (
+					id SERIAL PRIMARY KEY,
+					book_id INTEGER NOT NULL REFERENCES books(id),
+					number INTEGER NOT NULL
+				);
 
-		CREATE INDEX IF NOT EXISTS idx_chapters_book_id ON chapters(book_id);
-		`
-		_, err = DB.Exec(createChaptersTableCmd)
-		if err != nil {
-			slog.Debug("This is expected if the 'chapters' table already exists")
-		}
+				CREATE INDEX IF NOT EXISTS idx_chapters_book_id ON chapters(book_id);
+				`
+				_, err = DB.Exec(createChaptersTableCmd)
+				if err != nil {
+					slog.Debug("This is expected if the 'chapters' table already exists")
+				}
 
-		// Create Verses table
-		createVersesTableCmd := `
-		CREATE TABLE IF NOT EXISTS verses (
-			id SERIAL PRIMARY KEY,
-			chapter_id INTEGER NOT NULL REFERENCES chapters(id),
-			number INTEGER NOT NULL,
-			UNIQUE(chapter_id, number)
-		);
+				// Create Verses table
+				createVersesTableCmd := `
+				CREATE TABLE IF NOT EXISTS verses (
+					id SERIAL PRIMARY KEY,
+					chapter_id INTEGER NOT NULL REFERENCES chapters(id),
+					number INTEGER NOT NULL,
+					UNIQUE(chapter_id, number)
+				);
 
-		CREATE INDEX IF NOT EXISTS idx_verses_chapter_id ON verses(chapter_id);
-		`
-		_, err = DB.Exec(createVersesTableCmd)
-		if err != nil {
-			slog.Debug("This is expected if the 'verses' table already exists")
-		}
+				CREATE INDEX IF NOT EXISTS idx_verses_chapter_id ON verses(chapter_id);
+				`
+				_, err = DB.Exec(createVersesTableCmd)
+				if err != nil {
+					slog.Debug("This is expected if the 'verses' table already exists")
+				}
 
-		// Create Words table
-		createWordsTableCmd := `
-		CREATE TABLE IF NOT EXISTS words (
-			id SERIAL PRIMARY KEY,
-			verse_id INTEGER NOT NULL REFERENCES verses(id),
-			text VARCHAR(255) NOT NULL,
-			lemma VARCHAR(255),
-			strong VARCHAR(50),
-			morph VARCHAR(50)
-		);
+				// Create Words table
+				createWordsTableCmd := `
+				CREATE TABLE IF NOT EXISTS words (
+					id SERIAL PRIMARY KEY,
+					verse_id INTEGER NOT NULL REFERENCES verses(id),
+					text VARCHAR(255) NOT NULL,
+					lemma VARCHAR(255),
+					strong VARCHAR(50),
+					morph VARCHAR(50)
+				);
 
-		CREATE INDEX IF NOT EXISTS idx_words_verse_id ON words(verse_id);
-		`
-		_, err = DB.Exec(createWordsTableCmd)
-		if err != nil {
-			slog.Debug("This is expected if the 'words' table already exists")
-		}
+				CREATE INDEX IF NOT EXISTS idx_words_verse_id ON words(verse_id);
+				`
+				_, err = DB.Exec(createWordsTableCmd)
+				if err != nil {
+					slog.Debug("This is expected if the 'words' table already exists")
+				}
 
-		if !bookExists("Matthew") {
-			importBook(matthew)
-		}
 
-		if !bookExists("Mark") {
-			importBook(mark)
-		}
+		addTheBooks()
 
-		if !bookExists("Revelation") {
-			importBook(revelation)
-		}
 	*/
 
 	slog.Info("Database migrations completed")
@@ -305,3 +291,56 @@ type Word struct {
 	Strong  string `json:"strong"`
 	Morph   string `json:"morph"`
 }
+
+/*
+func addTheBooks() {
+
+	truncRows, err := DB.Query("truncate table books, chapters, verses, words")
+	if err != nil {
+		log.Fatalf("Error truncating tables: %v", err)
+	}
+	defer truncRows.Close()
+
+	booksMap := make(map[int]string)
+
+	dirEntries, err := booksFS.ReadDir("books")
+	if err != nil {
+		log.Fatalf("Error reading books directory: %v", err)
+	}
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			continue
+		}
+		fileName := entry.Name()
+
+		fileName = strings.TrimSuffix(fileName, ".json")
+		splitStr := strings.Split(fileName, "_")
+
+		orderStr := splitStr[0]
+		orderNum, err := strconv.Atoi(orderStr)
+		if err != nil {
+			log.Fatalf("Error converting order string to int: %v", err)
+		}
+
+		booksMap[orderNum] = splitStr[1]
+
+	}
+
+	for i := 1; i <= len(booksMap); i++ {
+		bookTitle := booksMap[i]
+		if !bookExists(bookTitle) {
+			data, err := booksFS.ReadFile("books/" + strconv.Itoa(i) + "_" + bookTitle + ".json")
+			if err != nil {
+				log.Fatalf("Error reading file %s: %v", bookTitle, err)
+			}
+			err = importBook(data)
+			if err != nil {
+				log.Fatalf("Error importing book %s: %v", bookTitle, err)
+			}
+			slog.Info("Successfully imported book data", "title", bookTitle)
+		} else {
+			slog.Debug("Book already exists in the database", "title", bookTitle)
+		}
+	}
+}
+*/
